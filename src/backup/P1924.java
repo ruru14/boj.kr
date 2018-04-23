00b70a75fd6f5525d086562f1eb23eb9637a9906
ruru14 <ljw2889@gmail.com>
2018-04-23 17:16:52 +0900

    create repository
    solve 1924

---
+++ src/backup/P1924.java
@@ -0,0 +1,27 @@
+package backup;
+
+import java.util.*;
+
+public class P1924 {
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		int m = input.nextInt();
+		int d = input.nextInt();
+		int[] month = {31,28,31,30,31,30,31,31,30,31,30,31};
+		String[] day = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
+		int dDay = 0;
+		
+		for(int i=1; i<m+1; i++){
+			if(m==i){
+				dDay = d%7;
+				break;
+			}
+			else{
+				d+=month[i-1];
+			}
+			dDay = d%7;
+		}
+		
+		System.out.println(day[dDay]);
+	}
+}
---
+++ src/baekjoon/Main.java
@@ -0,0 +1,12 @@
+package baekjoon;
+
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
